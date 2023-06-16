b8b6f88ca02569450d1a173459e2472618f5c8f2
Dan Skaggs <daskaggs@cisco.com>
2023-06-16 11:39:31 -0600

    Scaffolding view components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { NameListComponent } from './name-list/name-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    NameListComponent
   ],
   imports: [
     BrowserModule,
