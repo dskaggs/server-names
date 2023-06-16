08521e2bcbb0d6d79adcf3ca4930141a81b9ccb9
Dan Skaggs <daskaggs@cisco.com>
2023-06-16 13:58:43 -0600

    Retrieving JSON data via HttpClient on component init

--- src/app/name-list/name-list.component.ts
+++ src/app/name-list/name-list.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { ServerNamesService } from '../services/server-names.service'
+
 
 @Component({
   selector: 'app-name-list',
   templateUrl: './name-list.component.html',
   styleUrls: ['./name-list.component.css']
 })
-export class NameListComponent {
+export class NameListComponent{
+
+  constructor( serverNamesService : ServerNamesService ) {
+      serverNamesService.getList().subscribe()
+  }
 
 }
--- src/app/services/server-names.service.ts
+++ src/app/services/server-names.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http'
 @Injectable({
   providedIn: 'root'
 })
+
 export class ServerNamesService {
 
-  constructor() { }
+  private namesUrl = 'assets/data/names.json'
+
+  constructor( private http : HttpClient ) {
+    this.http = http
+   }
+
+  public getList(){
+
+    return this.http.get( this.namesUrl )
+
+   }
 }
